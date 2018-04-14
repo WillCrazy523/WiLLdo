5f53d23a73d9e4fa78a81770eeafc6f6e6fc2b86
Chao Wang <c.willwang@gmail.com>
2018-04-13 21:49:51 -0700

    Ready to add Search Bar Code

---
+++ WiLLdo/Controllers/WiLLdoListVC.swift
@@ -0,0 +1,133 @@
+//
+//  ViewController.swift
+//  WiLLdo
+//
+//  Created by Chao Wang on 4/13/18.
+//  Copyright © 2018 Chao Wang. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class WiLLdoListVC: UITableViewController {
+    
+    var itemArray = [Item]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        
+        loadItems()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //MARK - TableView Datasource Method
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        //Check mark
+        //Ternary Operator ==>
+        //value = condintion? valueIfTrue : ValueIfFalse
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        return cell
+    }
+    
+    //MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        //      context.delete(itemArray[indexPath.row])
+        //      itemArray.remove(at: indexPath.row)
+        
+        //check each row w/ or w/o check mark
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        self.saveItem()
+        
+        //falsh background when select
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK - Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todo Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what will happen once the user click the Add Item Button on out UIAlert
+            
+            let newItem = Item(context: self.context)
+            newItem.title = textField.text!
+            newItem.done = false
+            
+            self.itemArray.append(newItem)
+            
+            self.saveItem()
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Creat New Item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+        
+    }
+    
+    //MARK - Model Manupulation Method
+    
+    func saveItem() {
+        
+        do {
+            try context.save()
+        }
+        catch {
+            print("Error saving context \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadItems() {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        do {
+            itemArray = try context.fetch(request)
+        }
+        catch {
+            print("Error fetching data from context \(error)")
+        }
+        
+    }
+    
+}
+
+extension WiLLdoListVC: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+    }
+    
+    
+}
+
