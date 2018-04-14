96cb8708b32ae80914b05613a4021eb80b5e8083
Chao Wang <c.willwang@gmail.com>
2018-04-14 14:48:42 -0700

    Persisting Data using Core Data.

--- WiLLdo/Controllers/TodoListVC.swift
+++ WiLLdo/Controllers/TodoListVC.swift
@@ -9,9 +9,15 @@
 import UIKit
 import CoreData
 
-class WiLLdoListVC: UITableViewController {
+class TodoListVC: UITableViewController {
     
     var itemArray = [Item]()
+    
+    var selectedCategory : Category? {
+        didSet {
+            loadItems()
+        }
+    }
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
@@ -19,9 +25,7 @@ class WiLLdoListVC: UITableViewController {
         super.viewDidLoad()
         
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
-        
-        loadItems()
-        
+
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -76,6 +80,7 @@ class WiLLdoListVC: UITableViewController {
             let newItem = Item(context: self.context)
             newItem.title = textField.text!
             newItem.done = false
+            newItem.parentCategory = self.selectedCategory
             
             self.itemArray.append(newItem)
             
@@ -109,8 +114,17 @@ class WiLLdoListVC: UITableViewController {
         self.tableView.reloadData()
     }
     
-    func loadItems() {
-        let request: NSFetchRequest<Item> = Item.fetchRequest()
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+        
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+
+        if let addtionalPredicate = predicate {
+            
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, addtionalPredicate])
+        } else {
+            request.predicate = categoryPredicate
+        }
+        
         do {
             itemArray = try context.fetch(request)
         }
@@ -118,16 +132,49 @@ class WiLLdoListVC: UITableViewController {
             print("Error fetching data from context \(error)")
         }
         
+        tableView.reloadData()
+        
     }
     
 }
 
-extension WiLLdoListVC: UISearchBarDelegate {
+//MARK - Search Bar Method
+extension TodoListVC: UISearchBarDelegate {
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request, predicate: predicate)
+        
     }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+        }
+    }
+
     
     
 }
+
+
+
+
+
+
+
+
+
 
